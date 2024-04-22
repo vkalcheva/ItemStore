0cc55b7db8e78da5466706344cfc1e29ccc0d291
vkalcheva <violetakalcheva@abv.bg>
2024-04-22 14:08:59 +0300

    Create Flask App

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+store = [
+    {
+        "name": "My Store",
+        "items": [
+            {
+                "name": "Chair",
+                "price": 15.99
+            }
+        ]
+    }
+]
