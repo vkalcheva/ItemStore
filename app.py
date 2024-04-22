5e8880da0a4be2b9f1e24af6ae9c0481a0160d03
vkalcheva <violetakalcheva@abv.bg>
2024-04-22 14:19:16 +0300

    Create first API endpoint

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
-store = [
+stores = [
     {
         "name": "My Store",
         "items": [
@@ -13,3 +13,7 @@ store = [
         ]
     }
 ]
+
+@app.get("/store")
+def get_stores():
+    return {"stores": stores}
