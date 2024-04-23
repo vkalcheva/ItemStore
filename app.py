f52cabe5714dfaebb797d9d10108b880147bb437
vkalcheva <violetakalcheva@abv.bg>
2024-04-23 17:56:31 +0300

    Add error handling

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import uuid
 from flask import Flask, request
+from flask_smorest import abort
 from db import items, stores
 
 app = Flask(__name__)
@@ -22,7 +23,7 @@ def create_store():
 def create_item():
     item_data = request.get_json()
     if item_data["store_id"] not in stores:
-        return {"message": "Store not found"}, 404
+        abort(404, message="Store not found")
     
     item_id = uuid.uuid4().hex
     item = {**item_data, "id": item_id}
@@ -30,20 +31,20 @@ def create_item():
 
     return item, 201
 
-@app.get("/item")
-def get_all_itemes():
-    return {"items": list(items.values())}
-
 @app.get("/store/<string:store_id>")
 def get_store(store_id):
     try:
         return stores[store_id]
     except KeyError:
-        return {"message": "Store not found"}, 404
+        abort(404, message="Store not found")
 
 @app.get("/item/<string:item_id>")
 def get_item(item_id):
     try:
        return items[item_id] 
     except KeyError:
-        return {"message": "Item not found"}, 404
+        abort(404, message="Item not found")
+
+@app.get("/item")
+def get_all_itemes():
+    return {"items": list(items.values())}
