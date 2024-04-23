ecb160a71aaed7e09d7d600b2560b61c2c63cdfb
vkalcheva <violetakalcheva@abv.bg>
2024-04-23 14:51:15 +0300

    Improve item to store functionality

---
+++ db.py
@@ -0,0 +1,13 @@
+# stores = [
+#     {
+#         "name": "My Store",
+#         "items": [
+#             {
+#                 "name": "Chair",
+#                 "price": 15.99
+#             }
+#         ]
+#     }
+# ]
+stores = {}
+items = {}
