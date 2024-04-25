2f98e5da5995029b7545e46eacfad3be1a5a067f
vkalcheva <violetakalcheva@abv.bg>
2024-04-25 11:43:02 +0300

    Decorating responses with Flask-Smorest

--- resources/item.py
+++ resources/item.py
@@ -11,6 +11,7 @@ blp = Blueprint("Items", __name__, description="Operations on items")
 
 @blp.route("/item/<string:item_id>")
 class Item(MethodView):
+    @blp.response(200, ItemSchema)
     def get(self, item_id):
         try:
             return items[item_id]
@@ -25,6 +26,7 @@ class Item(MethodView):
             abort(400, message="Item not found.")
 
     @blp.arguments(ItemUpdateSchema)
+    @blp.response(200, ItemSchema)
     def put(self, item_data, item_id):
         try:
             item = items[item_id]
@@ -38,10 +40,12 @@ class Item(MethodView):
 
 @blp.route("/item")
 class ItemList(MethodView):
+    @blp.response(200, ItemSchema(many=True))
     def get(self):
-        return {"items": list(items.values())}
+        return items.values()
     
     @blp.arguments(ItemSchema)
+    @blp.response(201, ItemSchema)
     def post(self, item_data):
         for item in items.values():
             if (
