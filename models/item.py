1e2c685082896c529ccefffb5ffec6022b23931e
vkalcheva <violetakalcheva@abv.bg>
2024-04-27 10:28:46 +0300

    Create models

--- models/item.py
+++ models/item.py
@@ -1,3 +1,10 @@
 from db import db
 
+class ItemModel(db.Model):
+    __tablename__ = "items"
 
+    id = db.Column(db.Integer, primery_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    price = db.Column(db.Float(orecision=2), unique=False, nullable=False)
+    store_id = db.Column(db.Integer, unique=False, nullable=False)
+    
