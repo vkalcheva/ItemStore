3685d2f71643b3e883f284cfd47bd9628ffffccf
vkalcheva <violetakalcheva@abv.bg>
2024-04-27 11:31:59 +0300

    Configure SQLAlchemy

--- models/store.py
+++ models/store.py
@@ -3,7 +3,7 @@ from db import db
 class StoreModel(db.Model):
     __tablename__ = "stores"
 
-    id = db.Column(db.Integer, primery_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True, nullable=False)
     items = db.relationship("ItemModels", back_populates="store", lazy="dynamic")
     
--- resources/item.py
+++ resources/item.py
@@ -2,8 +2,6 @@ import uuid
 from flask import request
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
-
-from db import items
 from schemas import ItemSchema, ItemUpdateSchema
 
 blp = Blueprint("Items", __name__, description="Operations on items")
