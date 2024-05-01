c5b949e32476c1dc259446f7f9875ca2e72661cd
vkalcheva <violetakalcheva@abv.bg>
2024-05-01 19:28:08 +0300

    Create secondary table for many-to-many relationship

--- models/tag.py
+++ models/tag.py
@@ -4,8 +4,9 @@ from db import db
 class TagModel(db.Model):
     __tablename__ = "tags"
 
-    id = db.Column(db.Intiger, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True, nullable=False)
-    store_id = db.Column(db.Intiger, db.ForeignKey("store.id"), nullable=False)
+    store_id = db.Column(db.Integer, db.ForeignKey("stores.id"), nullable=False)
 
     store = db.relationship("StoreModel", back_populates="tags")
+    items = db.relationship("ItemModel", back_populate="tags", secondary="items_tags")
